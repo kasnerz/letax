e350c2d2639e3348aedecb3c59e46ad8ee38473f
Zdenek Kasner <zdenek.kasner@gmail.com>
2023-08-12 22:34:22 +0200

    Add email

--- src/app/pages/7_🔒️_Stránka účastníka.py
+++ src/app/pages/7_🔒️_Stránka účastníka.py
@@ -445,8 +445,9 @@ def show_info_info():
     st.markdown(
         f"""
         #### Kontakty
-        - **krizové číslo (dostupné 24/7)**: {crisis_number}
+        - **dotazy a připomínky**: letni@x-challenge.cz
         - **happiness manager**: {happiness_manager}
+        - **krizové číslo (dostupné 24/7)**: {crisis_number}
 
         #### Odkazy
         - **pravidla akce**: https://x-challenge.cz/letni/pravidla/
