89976f1d622020f96ceaaaf9895e3fbc1d11e59e
Zdeněk Kasner <zdenek.kasner@gmail.com>
2024-08-11 08:08:14 +0000

    Log memory to file

--- run.sh
+++ run.sh
@@ -42,12 +42,12 @@ else
         MEMORY_USAGE_MB=$((MEMORY_USAGE_KB / 1024))
 
         if [ $MEMORY_USAGE_MB -gt $MEMORY_LIMIT_MB ]; then
-            echo "$TIMESTAMP - Memory usage is over the limit: $MEMORY_USAGE_MB MB. Rebooting..."
+            echo "$TIMESTAMP - Memory usage is over the limit: $MEMORY_USAGE_MB MB. Rebooting..." > memory.log
             killall -9 streamlit
             eval $CMD &
         else
             if [ $VERBOSE ]; then
-                echo "$TIMESTAMP - Memory usage is within limits: $MEMORY_USAGE_MB MB."
+                echo "$TIMESTAMP - Memory usage is within limits: $MEMORY_USAGE_MB MB." > memory.log
             fi
         fi
         sleep $REFRESH_RATE
